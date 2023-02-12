bee58bca7d5e79669b7e1269b873aa4d9af088af
Shazreen Seatter <shazseatter12@gmail.com>
2023-02-12 21:16:25 +0000

    spelling change

--- models/book_list.py
+++ models/book_list.py
@@ -2,7 +2,7 @@ from models.book import *
 
 
 book_1 = Book("The Lord of the Rings", "T R R Tolkien", "Fantasy", True)
-book_2 = Book("A Court of Torns of Roses", "Sarah J Maas", "Fantasy", False)
+book_2 = Book("A Court of Thorns of Roses", "Sarah J Maas", "Fantasy", False)
 book_3 = Book("Pen Pal", "J.T Geissinger", "Thriller", True)
 
 book_list = [book_1, book_2, book_3]
